922bf6dd35a6af6cd9341965ef8e1253ad9f5a93
Andres Pontt <none@none.com>
2020-08-19 22:50:19 -0400

    new page

---
+++ precache-manifest.9824ecfb65726a18925cdf43c9c6a9bc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "653def467da43d335294317aacbf60c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b44017a99c8d05052fc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0b99a059e6cf11c74bd4",
+    "url": "/static/js/2.a552fb7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a552fb7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b44017a99c8d05052fc",
+    "url": "/static/js/main.4e779051.chunk.js"
+  },
+  {
+    "revision": "fb8cfbe2e5f29fa37fc0",
+    "url": "/static/js/runtime-main.717f4f89.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
