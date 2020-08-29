dede5faa6673b81c7ffc190f837e8f6bad3e9651
andrespontt <andrespontt@gmail.com>
2020-08-29 23:54:34 +0000

    Updates

---
+++ precache-manifest.f87af9d0d9e212efe6cc039c7891acf2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db22579c37bb9d5df47bcd2ae933ea78",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e50fc22cb4986ad76f1e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "10aa3d60867f429e1e7c",
+    "url": "/static/js/2.78e3d942.chunk.js"
+  },
+  {
+    "revision": "7c6de3322eb4ea2ab32454492afd4f37",
+    "url": "/static/js/2.78e3d942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50fc22cb4986ad76f1e",
+    "url": "/static/js/main.a3102fc0.chunk.js"
+  },
+  {
+    "revision": "6cb6ff9f918c448a2dd5",
+    "url": "/static/js/runtime-main.67585a02.js"
+  }
+]);
