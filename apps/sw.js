f3b0bea0aef790c1a18cde225d6a82b5a1dd4550
Andres Pontt <andrespontt@users.noreply.github.com>
2025-11-26 23:02:58 -0500

    feat: Add new Pomodoro timer application with PWA support, including service worker and manifest.

---
+++ apps/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'pomodoro-v1';
+const ASSETS = [
+  './pomodoro.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
