7d6ccbcd312a0779616fea850f2cdc932cf8083a
Andres Pontt <andrespontt@users.noreply.github.com>
2026-08-22 18:17:37 -0400

    Add Loopdeck YouTube looper PWA

---
+++ apps/loopdeck-sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'loopdeck-v5';
+const APP = [
+  '/apps/loopdeck.html',
+  '/apps/loopdeck.webmanifest',
+  '/apps/assets/loopdeck-icon.svg'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('loopdeck-') && key !== CACHE).map(key => caches.delete(key)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin !== location.origin) return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request).then(hit => hit || caches.match('/apps/loopdeck.html'))));
+});
